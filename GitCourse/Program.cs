d2fd05f5fd171553521dec9fa912e7c6b6385de8
Porfirev A.A 4118 <2002wasj@mail.ru>
2022-11-03 14:51:14 +0300

    first commit

---
+++ GitCourse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+            }
+            
+        }
+    }
+}
